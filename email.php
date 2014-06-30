11d65f22d48e9b6e3f4d82a5aa89625891a17e1e
juan2ramos <juan2ramos@gmail.com>
2014-06-30 15:33:12 -0500

    + Formulario de contacto

---
+++ email.php
@@ -0,0 +1,36 @@
+<?php
+
+if (!empty($_POST)) {
+
+    if(empty($_POST['name']) || empty($_POST['comment'])|| empty($_POST['phone']) || empty($_POST['dirigido'])){
+        $arrayMsj['success'] = FALSE;
+        $arrayMsj['message'] = 'Todos los campos son requeridos';
+        echo (json_encode($arrayMsj));
+        exit;
+    }
+
+    require '/home/redlat/public_html/via/include/PHPMailer/PHPMailerAutoload.php';
+
+    $mail = new PHPMailer;
+    $mail->IsSendmail();
+    $mail->FromName ='Formulario Circulart' ;
+    $mail->CharSet = 'UTF-8';
+    $mail->From = 'info@circulart.org';
+    $mail->Subject = 'Mensaje de circulart Web';
+    $mail->MsgHTML('Mensaje con HTML');
+    $template = '<h1>Mensaje enviado desde el formulario de Circulart</h1><br><br>';
+    $template .= 'Nombre: ' . $_POST['name'] . '<br>';
+    $template .= 'Email: ' . $_POST['email'] .'<br>';
+    $template .= 'telefono: ' . $_POST['phone'] .'<br>';
+    $template .= 'Mensaje: <br>' . $_POST['comment'] ;
+    $mail->Body = $template;
+    $mail->AddAddress($_POST['dirigido'], '');
+    $mail->Send();
+
+    $arrayMsj['success'] = TRUE;
+    $arrayMsj['message'] = 'Felicitaciones, su mensaje a sido enviado con éxito!!';
+    echo (json_encode($arrayMsj));
+    exit;
+}else{
+    echo "Error al intentar acceder";
+}
